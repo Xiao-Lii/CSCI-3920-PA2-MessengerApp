8844198bbdfaf8f796d6ee349804cc1234e95568
Xiao-Lii <63008453+Xiao-Lii@users.noreply.github.com>
2020-11-27 12:25:48 -0700

    Adding some Server functions
    Filled in very basic constructors for Message, User, and Messenger App

---
+++ src/Users.py
@@ -0,0 +1,7 @@
+class user:
+    def __init__(self, email: str, password: str, username : str):
+        self.__email = email
+        self.__password = password
+        self.__username = username
+
+
