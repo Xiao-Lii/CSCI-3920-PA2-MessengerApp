9e1e06b16b11d22801a62692586926e449a974bd
Xiao-Lii <63008453+Xiao-Lii@users.noreply.github.com>
2020-12-02 21:33:55 -0700

    Merging Lee's & Rin's Code

--- src/Server.py
+++ src/Server.py
@@ -4,26 +4,25 @@ from threading import Thread
 
 socket_list = []
 
-
 def send_message(msg: str, client_socket: socket):
     print(f"""[SRV] SEND >> {msg}""")
     client_socket.send(msg.encode("UTF-16"))
     pass
 
-
 def receive_message(client_socket: socket, max_length: int = 1024):
-    # try:
-    #     message_header = client_socket.recv(10)
-    #     if not len(message_header):
-    #         return False
-    #
-    #     message_length = int(message_header.decode("UTF-8"))
-    #     if message_length < 1025:
-    #         return {"header": message_header, "data": client_socket.recv(message_length)}
-    #
-    # except:
-    #     return False
+    """ # For some weird reason, the try block causes errors with receiving the message
+    try:
+        message_header = client_socket.recv(10)
+        if not len(message_header):
+            return False
 
+        message_length = int(message_header.decode("UTF-8").strip())
+        if message_length < 1025:
+            return {"header": message_header, "data": client_socket.recv(message_length)}
+
+    except:
+        return False
+    """
     msg = client_socket.recv(max_length).decode("UTF-16")
     print(f"""[SRV] RCV >> {msg}""")
     return msg
@@ -52,7 +51,8 @@ class Server(Thread):
 
             client_runs = True
             while client_runs:
-                client_message = receive_message(client_socket)
+
+                client_message = str(receive_message(client_socket))
                 if client_message == "QUIT":
                     client_runs = False
                     send_message("OK", client_socket)
@@ -67,39 +67,47 @@ class Server(Thread):
 
         server_socket.close()
 
-    def start_service_menu(self):
-        service_menu = "1. Load data from file.\n" \
-                       "2. Start the messenger service\n" \
-                       "3. Stop the messenger service\n" \
-                       "4. Save data to file\n"
+    """
+    def start_service_menu(self, client_socket:socket):
+        service_menu =  "1. Load data from file.\n" \
+                        "2. Start the messenger service\n" \
+                        "3. Stop the messenger service\n" \
+                        "4. Save data to file\n"
         print(service_menu)
-        option = input(int())
+        displayStart = True
+        while displayStart:
+            # 1st we want to keep displaying the Start Menu to the User
+            client_socket.send(service_menu.encode("UTF-16"))
+            client_message = str(receive_message(client_socket))
+            # END OF RECEIVE MESSAGE
 
-        if option == 1:
-            # READ THAT FILE
-            print("Inside option 1")  # Test Filler - May delete
-        elif option == 2:
-            # BETTER START THAT MESSENGER
-            # MAYBE FOR RIGHT HERE, WE WANT:
-            #       1. MAYBE TO CREATE A NEW MESSAGING SYSTEM OBJECT IF ONE WASN'T LOADED IN
-            #       2. CALL FUNCTION TO OPEN THE NEXT MENU - USER LOGIN
-            print("Inside option 2")  # Test Filler - May delete
-        elif option == 3:
-            # BETTER STOP THAT MESSENGER
-            print("Inside option 3")  # Test Filler - May delete
-        elif option == 4:
-            # SAVE THE FILE
-            print("Inside option 4")  # Test Filler - May delete
-        else:
-            # DONE GOOF NOW - ERROR
-            print("Error: Invalid menu selection")
+
+            if client_message == 1:
+                # READ THAT FILE
+                print("Inside option 1")            # Test Filler - May delete
+            elif client_message == 2:
+                # BETTER START THAT MESSENGER
+                # MAYBE FOR RIGHT HERE, WE WANT:
+                #       1. MAYBE TO CREATE A NEW MESSENGING SYSTEM OBJECT IF ONE WASN'T LOADED IN
+                #       2. CALL FUNCTION TO OPEN THE NEXT MENU - USER LOGIN
+                print("Inside option 2")            # Test Filler - May delete
+            elif client_message == 3:
+                # BETTER STOP THAT MESSENGER
+                print("Inside option 3")            # Test Filler - May delete
+            elif client_message == 4:
+                # SAVE THE FILE
+                print("Inside option 4")            # Test Filler - May delete
+            else:
+                # DONE GOOF NOW - ERROR
+                print("Error: Invalid menu selection")
+    """
 
     def sign_up(self, messengerApp, username, password):
         # We want to access the messengerApp's current list of Users
         # 1. Check if the username/email exists in our list of registered users?
-        # If so, success = Create an account, call User Constructor
-        # If not, display error
-        return  # WE DON'T NEED THIS - MAY DELETE LATER
+            # If so, success = Create an account, call User Constructor
+            # If not, display error
+        return # WE DON'T NEED THIS - MAY DELETE LATER
 
     def sign_in(self, messengerApp, username, password):
         # We want to access the messengerApp's current list of Users
@@ -107,9 +115,6 @@ class Server(Thread):
         # 2. Is the password correct?
         # If so, success = Create a thread for Client
         # If not, display error
-        return  # WE DON'T NEED THIS - MAY DELETE LATER
+        return # WE DON'T NEED THIS - MAY DELETE LATER
 
-    # TODO:
-    #   Regarding to #4 (Considerations - example):
-    #   socket can only connect to one port.
-    #   I'm assuming that this is where the threading comes in
+
